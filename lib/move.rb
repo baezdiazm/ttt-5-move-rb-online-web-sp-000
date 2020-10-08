ab87d26e5ebcaeab6755fa09aaacfb2b5b9ba4c7
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-10-08 04:29:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input = 0)
+def input_to_index(input.to_i)
 
 end
 # code your input_to_index and move method here!
