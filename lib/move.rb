ed180da69a9aa979c259be8a9c03bcf6bb4a11c3
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-10-08 04:37:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
   converted
 end
 
-def move(arg1, arg2, arg3 = "X")
-
+def move(board, move, token = "X")
+  board[move] = token
 end
 # code your input_to_index and move method here!
